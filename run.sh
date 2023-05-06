74bcb85543bdb18de20395565bdc2e1a7f5121d1
undg <und3rdg@gmail.com>
2023-05-06 23:42:02 +0100

    Fix run script, after extracting dependencies from OS into api repo.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
- uvicorn main:app --host 192.168.1.222 --port 8448 --app-dir pulse-remote-api & yarn --cwd pulse-remote-react start
+cd pulse-remote-api
+./run.sh & yarn --cwd ../pulse-remote-react start
+# uvicorn main:app --host 192.168.1.222 --port 8448 --app-dir pulse-remote-api & yarn --cwd ../pulse-remote-react start
+cd -
 
