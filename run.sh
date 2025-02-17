7b72b0c1f7e97620f8f147b5539fa216bb16a3ac
undg <und3rdg@gmail.com>
2025-02-17 17:26:03 +0000

    tidy

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd pulse-remote-api
-./run.sh & yarn --cwd ../pulse-remote-react start
-# uvicorn main:app --host 192.168.1.222 --port 8448 --app-dir pulse-remote-api & yarn --cwd ../pulse-remote-react start
-cd -
-
